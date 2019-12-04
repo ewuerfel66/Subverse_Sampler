7cbd1a1b0d9261e5c9b8099e43dffdd8034dd64c
Eric Wuerfel <Eric@Erics-MacBook-Pro-5.local>
2019-12-04 17:06:34 -0500

    added Fox

--- scraper.py
+++ scraper.py
@@ -301,7 +301,7 @@ print("--- Connecting...")
 # Credentials
 dbname = "iuawmtcy"
 user = "iuawmtcy"
-password = "UnWCQ5-4ymEJCpY5Tly-F7ZXXONAEx7i" # Don't commit!!!
+password = "" # Don't commit!!!
 host = "salt.db.elephantsql.com"
 
 # Establish connection
