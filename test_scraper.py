b4d4a938955e3690ad4332b3c14ee54acaefba2b
Eric Wuerfel <Eric@Erics-MacBook-Pro-5.local>
2019-12-05 16:58:00 -0500

    added nypost, 10 sources

--- test_scraper.py
+++ test_scraper.py
@@ -93,6 +93,7 @@ nypost_df["year"] = nypost_years
 # Concatenate the dfs
 df = pd.concat([nypost_df])
 
+print(nypost_articleURLs[20:26])
 print(len(nypost_articleURLs))
 print("")
 
@@ -101,46 +102,46 @@ print("")
 ############## Send to ElephantSQL##################
 ####################################################
 
-print("Sending to DataBase:")
+# print("Sending to DataBase:")
 
-print("--- Connecting...")
-# Credentials
-dbname = "iuawmtcy"
-user = "iuawmtcy"
-password = "UnWCQ5-4ymEJCpY5Tly-F7ZXXONAEx7i" # Don't commit!!!
-host = "salt.db.elephantsql.com"
+# print("--- Connecting...")
+# # Credentials
+# dbname = "iuawmtcy"
+# user = "iuawmtcy"
+# password = "" # Don't commit!!!
+# host = "salt.db.elephantsql.com"
 
-# Establish connection
-pg_conn = psycopg2.connect(dbname=dbname, user=user,
-    password=password, host=host)
+# # Establish connection
+# pg_conn = psycopg2.connect(dbname=dbname, user=user,
+#     password=password, host=host)
 
-# Instantiate cursor
-pg_curs = pg_conn.cursor()
+# # Instantiate cursor
+# pg_curs = pg_conn.cursor()
 
-# Clean data for db insertion
-print('--- Getting the data ready...')
-dirty_rows = df.values
+# # Clean data for db insertion
+# print('--- Getting the data ready...')
+# dirty_rows = df.values
 
-# Clean up rows
-rows = []
+# # Clean up rows
+# rows = []
 
-for row in dirty_rows:
-    rows.append(tuple(row))
+# for row in dirty_rows:
+#     rows.append(tuple(row))
 
-print('--- Adding data to DataBase...')
-# Loop over the array to write rows in the DB
-for row in rows:
-    insert = """
-    INSERT INTO news
-    (article_URL, source, day, month, year)
-    VALUES 
-    """ + str(row) + ';'
+# print('--- Adding data to DataBase...')
+# # Loop over the array to write rows in the DB
+# for row in rows:
+#     insert = """
+#     INSERT INTO news
+#     (article_URL, source, day, month, year)
+#     VALUES 
+#     """ + str(row) + ';'
     
-    pg_curs.execute(insert)
+#     pg_curs.execute(insert)
 
-# Save and finish session
-pg_curs.close()
-pg_conn.commit()
+# # Save and finish session
+# pg_curs.close()
+# pg_conn.commit()
 
-print("")
-print('all done!')
+# print("")
+# print('all done!')
