fafcc78fabc86e5997ab757062ed8d14f31d20f8
eric <eric97@vt.edu>
2019-12-31 13:20:16 -0500

    checked scrapers and changed DB credentials

--- sources.py
+++ sources.py
@@ -5,7 +5,7 @@ A Class for each Source
 import pandas as pd
 
 class Source:
-    def __init__(self, name, codename, sections, scraper, article_URLs=[], daily_df=pd.DataFrame()):
+    def __init__(self, name, codename, sections, scraper, article_URLs=[], section_URLs=[], daily_df=pd.DataFrame()):
         self.name = name
         self.codename = codename
         self.sections = sections
